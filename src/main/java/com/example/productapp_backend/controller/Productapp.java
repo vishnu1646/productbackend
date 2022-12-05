69a986c9f0917d09f31e3df107eba36695fb7a2f
vishnu1646 <vishnulance98@gmail.com>
2022-12-05 17:27:34 +0530

    home

--- src/main/java/com/example/productapp_backend/controller/Productapp.java
+++ src/main/java/com/example/productapp_backend/controller/Productapp.java
@@ -1,4 +1,15 @@
 package com.example.productapp_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Productapp {
+    @GetMapping("/")
+    private String Homepage(){
+        return "Welcome to Product Home page";
+    }
+
+
+
 }
