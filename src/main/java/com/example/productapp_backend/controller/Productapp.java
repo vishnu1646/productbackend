770483d8aaf8f91b771578da781df0614aa3289d
vishnu1646 <vishnulance98@gmail.com>
2022-12-06 08:00:18 +0530

    search

--- src/main/java/com/example/productapp_backend/controller/Productapp.java
+++ src/main/java/com/example/productapp_backend/controller/Productapp.java
@@ -1,15 +1,23 @@
 package com.example.productapp_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Productapp {
     @GetMapping("/")
-    private String Homepage(){
+    public String Homepage(){
         return "Welcome to Product Home page";
     }
-
+    @PostMapping("/add")
+    public String  add(){
+        return "Adding Product";
+    }
+    @PostMapping("/search")
+    public String search(){
+        return "Searching Product";
+    }
 
 
 }
